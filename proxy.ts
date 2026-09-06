95704816ae69541518ccc85e5e57a687a5ad7b65
priyanshi20045 <146551763+priyanshi20045@users.noreply.github.com>
2026-09-06 19:58:05 +0530

    fix auth configuration

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,6 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
+import { auth } from '@/auth';
 
-export default NextAuth(authConfig).auth;
+export default auth;
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
