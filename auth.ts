9856041306619052a39c39676ce38c18d1998c45
priyanshi20045 <146551763+priyanshi20045@users.noreply.github.com>
2026-09-06 19:44:00 +0530

    fix auth handlers and trusted host

--- auth.ts
+++ auth.ts
@@ -20,7 +20,7 @@ async function getUser(email: string): Promise<User | undefined> {
   }
 }
 
-export const { auth, signIn, signOut } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
    trustHost: true,
   providers: [
     Credentials({
